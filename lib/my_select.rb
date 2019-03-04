d67034205a868c482b90442e708bd3bdb597d88a
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-04 23:53:51 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,13 @@ def my_select(collection)
  # your code here!
  if block_given?
    i=0
+   new_collection = Array.new
    while i<collection.length
-
+     
     yield(collection[i])
+    if collection[i] = num
+      new_collection<<collection[i]
+    end
      i=i+1
    end
 
